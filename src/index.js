11bf4259f1ea92c43568093c5dac43864719bbf9
CassiaSantos <cassia.oliveira.profissional@gmail.com>
2025-03-11 14:51:45 -0300

    Gerando blocos

--- src/index.js
+++ src/index.js
@@ -22,7 +22,42 @@ async function rollDice(){
     //Math.random = função que gera números de aleatórios de 0 a 1 (por isso que será multiplicado por 6 e depois somado com 1);
 }
 
+//Função para sorteador um bloco:
+async function getRandomBlock() {
+    let random = Math.random();
+    let result;
+
+    switch (true) {
+        case random < 0.33:
+            result = "RETA"
+            break;
+
+        case random < 0.66:
+            result = "CURVA"
+            break;
+    
+        default:
+            result = "CONFRONTO"
+            break;
+    }
+
+    return result;
+}
+
+async function playRaceEngine(character1, character2) {
+    for(let round = 1; round <= 5; round++){
+        console.log(`🏁 Rodada ${round}`)
+
+        //Sorteador bloco:
+        let block = await getRandomBlock();
+        console.log(`Bloco: ${block}`);
+    }
+}
+
 // Função Main autoinvocada:
 (async function main () {
     console.log(`🚨🏁 Corrida entre ${player1.NOME} e ${player2.NOME} começando... \n`);
+
+    //Function chains - chamada de função por dentro de outra: 
+    await playRaceEngine(player1, player2);
 })();
